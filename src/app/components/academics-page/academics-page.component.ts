f96949acbd39b59833675a59a9bc39017578995c
waglarry <emmanuelacquaah2468@gmail.com>
2024-06-16 12:51:55 +0000

    feat: create academics page component

---
+++ src/app/components/academics-page/academics-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-academics-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './academics-page.component.html',
+  styleUrl: './academics-page.component.scss'
+})
+export class AcademicsPageComponent {
+
+}
