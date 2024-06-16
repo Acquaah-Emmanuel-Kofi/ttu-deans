2eba48e2f69d2bf291edd4be4aafe5ed070b9a23
waglarry <emmanuelacquaah2468@gmail.com>
2024-06-16 01:27:07 +0000

    feat: integrate about page to homepage component

--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 import { IHeroCards } from './homepage.interface';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, AboutComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
