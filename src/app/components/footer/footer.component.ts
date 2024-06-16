ed3d7a6cb14e3fa66d4bfb7a4dc43692733da34c
waglarry <emmanuelacquaah2468@gmail.com>
2024-06-16 12:37:10 +0000

    feat: render quick links on footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { QuickLinks } from './footer.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+    public quickLinks: QuickLinks[] = [
+      {
+        label: 'Acadamc Calendar',
+        link: 'https://ttu.edu.gh/academic-calendar-20222023/'
+      },
+      {
+        label: 'University Library',
+        link: 'https://ttu.edu.gh/academic-calendar-20222023/'
+      },
+      {
+        label: 'Faculties and Departments',
+        link: 'https://ttu.edu.gh/academic-calendar-20222023/'
+      },
+      {
+        label: 'Student Resources',
+        link: 'https://ttu.edu.gh/academic-calendar-20222023/'
+      },
+      {
+        label: 'Student Resources Centre',
+        link: 'https://ttu.edu.gh/academic-calendar-20222023/'
+      },
+      {
+        label: 'Alumni',
+        link: 'https://ttu.edu.gh/academic-calendar-20222023/'
+      },
+    ];
 }
