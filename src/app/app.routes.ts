5a4b3963e4f62a638409e873a952a94ed7953846
waglarry <emmanuelacquaah2468@gmail.com>
2024-06-29 00:33:42 +0000

    refactor: change acemic route now

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,7 +13,7 @@ export const routes: Routes = [
         component: HomepageComponent
       },
       {
-        path: 'academic-programs-faculty-and-departments',
+        path: 'academic-programs',
         component: AcademicsPageComponent
       },
 ];
