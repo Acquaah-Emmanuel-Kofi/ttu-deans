eaae4644c72bb23ef7824db9339309964d682c17
waglarry <emmanuelacquaah2468@gmail.com>
2024-08-17 11:47:53 +0000

    feat: get content as an input in the department details component

--- src/app/shared/components/department-details/department-details.component.ts
+++ src/app/shared/components/department-details/department-details.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { ContentItem } from './department-details.interface';
 
 @Component({
   selector: 'app-department-details',
   standalone: true,
   imports: [],
   templateUrl: './department-details.component.html',
-  styleUrl: './department-details.component.scss'
+  styleUrl: './department-details.component.scss',
 })
 export class DepartmentDetailsComponent {
-
+  content = input<ContentItem[]>();
 }
