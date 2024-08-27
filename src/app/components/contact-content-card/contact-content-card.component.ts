fc958ab4e842bdfdfb713f8e4613b1336093f52b
waglarry <emmanuelacquaah2468@gmail.com>
2024-08-27 19:10:29 +0000

    feat: create contact-content-card

---
+++ src/app/components/contact-content-card/contact-content-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-content-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-content-card.component.html',
+  styleUrl: './contact-content-card.component.scss'
+})
+export class ContactContentCardComponent {
+
+}
