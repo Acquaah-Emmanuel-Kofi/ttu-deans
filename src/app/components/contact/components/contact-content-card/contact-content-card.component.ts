92f02d90c59e9eb1319871d493e6bfa89ae64b87
waglarry <emmanuelacquaah2468@gmail.com>
2024-08-29 09:27:49 +0000

    feat: design contact content card ui

--- src/app/components/contact/components/contact-content-card/contact-content-card.component.ts
+++ src/app/components/contact/components/contact-content-card/contact-content-card.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-contact-content-card',
   standalone: true,
   imports: [],
   templateUrl: './contact-content-card.component.html',
-  styleUrl: './contact-content-card.component.scss'
+  styleUrl: './contact-content-card.component.scss',
 })
 export class ContactContentCardComponent {
-
+  label = input.required<string>();
+  iconSrc = input.required<string>();
+  ref = input<string>();
 }
