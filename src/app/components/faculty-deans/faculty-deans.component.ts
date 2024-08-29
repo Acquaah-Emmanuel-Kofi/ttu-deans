2c80f245f9464096560ab6326a1674d2fec2491c
waglarry <emmanuelacquaah2468@gmail.com>
2024-08-29 22:43:23 +0000

    feat: structure page with title

--- src/app/components/faculty-deans/faculty-deans.component.ts
+++ src/app/components/faculty-deans/faculty-deans.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { PageTitleComponent } from '../../shared/components/page-title/page-title.component';
 
 @Component({
   selector: 'app-faculty-deans',
   standalone: true,
-  imports: [],
+  imports: [PageTitleComponent],
   templateUrl: './faculty-deans.component.html',
-  styleUrl: './faculty-deans.component.scss'
+  styleUrl: './faculty-deans.component.scss',
 })
 export class FacultyDeansComponent {
-
+  title: string = 'Faculty Deans';
 }
