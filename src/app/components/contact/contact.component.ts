c3a8ae761f819cfd3637cdf4881a8c233ed32fa8
waglarry <emmanuelacquaah2468@gmail.com>
2024-08-29 09:46:41 +0000

    feat: design contact form on page

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { InputFieldComponent } from './components/input-field/input-field.component';
+import { ContactContentCardComponent } from './components/contact-content-card/contact-content-card.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    InputFieldComponent,
+    ContactContentCardComponent,
+    ReactiveFormsModule,
+  ],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  form = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    subject: new FormControl('', [Validators.required]),
+    message: new FormControl('', [Validators.required]),
+  });
 
+  handleSubmit() {
+    console.log(this.form.value);
+    
+  }
 }
